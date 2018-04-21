859d7c226bf5506dc1e0d2752dde5a11c07ef72e
kcwoolverton <kcwoolverton@gmail.com>
2018-04-21 16:36:34 -0700

    Evan: Additional print statements added to python.

--- babel_functions.py
+++ babel_functions.py
@@ -1,9 +1,6 @@
 import string
 import random
 import sys
-# magic to fix broken pipe
-from signal import signal, SIGPIPE, SIG_DFL
-signal(SIGPIPE, SIG_DFL)
 
 length_of_page = 3239
 loc_mult = pow(30, length_of_page)
@@ -139,6 +136,9 @@ def main():
             # pad the length of the string to be 2 * block_length
             current_length = len(final_str)
             final_str = final_str + (' ' * ((2 * 4096) - current_length))
+
+            print("Modified final str is: " + final_str)
+            print("Modified length of final str i: " + str(len(final_str)))
             answer.write(final_str)
             answer.flush()
 
